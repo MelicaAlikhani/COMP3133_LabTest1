300002ac2536d456bfcf46d9506e78947608af56
Melica <94705804+MelicaAlikhani@users.noreply.github.com>
2026-02-11 19:24:50 -0500

    Fixed server initialization order

--- server.js
+++ server.js
@@ -1,55 +1,29 @@
 require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 const http = require("http");
 const socketio = require("socket.io");
-const mongoose = require("mongoose");
-const Message = require("./models/Message");
+
 const authRoutes = require("./routes/authRoutes");
-
-
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+// Middleware
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/api", authRoutes);
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.use(express.static("public"));
-
-app.use("/api", authRoutes);
-
 // Socket connection
 io.on("connection", (socket) => {
   console.log("New user connected");
-
-  socket.on("joinRoom", ({ username, room }) => {
-    socket.join(room);
-
-    io.to(room).emit("message", {
-      username: "System",
-      text: `${username} has joined the room`
-    });
-  });
-
-  socket.on("chatMessage", async ({ username, room, text }) => {
-    console.log("Message received:", text);
-
-    const newMessage = new Message({
-      username,
-      room,
-      text
-    });
-
-    await newMessage.save();
-
-    io.to(room).emit("message", {
-      username,
-      text
-    });
-  });
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
